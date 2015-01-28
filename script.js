0404623bcf3fa8d516b88ecb2173524aa1a23105
Ben Zweig <benzilla@me.com>
2015-01-28 10:22:53 -0800

    fix

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var colorPick = Math.floor((Math.random() * 6) + 1);
       var body=document.getElementsByTagName('body')[0];
       var ss = document.styleSheets[0];
+      ss.insertRule('.coloredText { font-size: 0.5em; }', 0);
       console.log(colorPick);
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
       if(colorPick==1){
-    ss.insertRule('.coloredText { font-size: 0.5em; }', 0);
+
     console.log("hey!");
     
     document.body.style.cursor = "url(./images/cursors/yellow_icon.png) 0 68, auto";
