20c5fecd1db88fa85a54e10beaba93779f1a36c0
Ben Zweig <benzilla@me.com>
2015-01-28 00:01:43 -0800

    better text

--- script.js
+++ script.js
@@ -12,27 +12,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("hey!");
     
     document.body.style.cursor = "url(./images/cursors/yellow_icon.png) 0 68, auto";
-    ss.insertRule('.title::selection { background: #E4FF00; }', 0);
+    ss.insertRule('.coloredText::selection { background: #E4FF00; }', 0);
 
   }
   if(colorPick==2){
     document.body.style.cursor = "url(./images/cursors/green_icon.png) 0 68, auto";
-    ss.insertRule('.title::selection { background: #7CFB6D; }', 0);
+    ss.insertRule('.coloredText::selection { background: #7CFB6D; }', 0);
   }
   if(colorPick==3){
     document.body.style.cursor = "url(./images/cursors/blue_icon.png) 0 68, auto";
-    ss.insertRule('.title::selection { background: #4CC5F9; }', 0);
+    ss.insertRule('.coloredText::selection { background: #4CC5F9; }', 0);
   }
   if(colorPick==4){
     document.body.style.cursor = "url(./images/cursors/pink_icon.png) 0 68, auto";
-    ss.insertRule('.title::selection { background: #FA609B; }', 0);
+    ss.insertRule('.coloredText::selection { background: #FA609B; }', 0);
   }
   if(colorPick==5){
     document.body.style.cursor = "url(./images/cursors/purple_icon.png) 0 68, auto";
-    ss.insertRule('.title::selection { background: #A44EF0; }', 0);
+    ss.insertRule('.coloredText::selection { background: #A44EF0; }', 0);
   }
   if(colorPick==6){
     document.body.style.cursor = "url(./images/cursors/orange_icon.png) 0 68, auto";
-    ss.insertRule('.title::selection { background: #FDAB36; }', 0);
+    ss.insertRule('.coloredText::selection { background: #FDAB36; }', 0);
   }
 });
