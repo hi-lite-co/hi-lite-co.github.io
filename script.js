577c5f105392ec776b05b82c20e0c0e0f1d33669
Ben Zweig <benzilla@me.com>
2015-01-28 11:53:23 -0800

    LOL

--- script.js
+++ script.js
@@ -8,45 +8,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
       var ss = document.styleSheets[0];
       console.log(colorPick);
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-      if(colorPick==1){
+    document.getElementById("pageTitle").innerHTML = '<span class="coloredText" id="colorSpan">FUCK</span>';
+    if(colorPick==1){
     console.log("hey!");
     
-    document.body.style.cursor = "url(./images/cursors/yellow_icon.png) 0 68, auto";
-    ss.insertRule('.coloredText { background: #E4FF00; }', 0);
-    ss.insertRule('a { cursor: url(./images/cursors/yellow_icon.png) 0 68, auto; }', 0);
-    document.getElementById("logoPic").src = "./images/logos/yellow@2x.png";
+      document.body.style.cursor = "url(./images/cursors/yellow_icon.png) 0 68, auto";
+      ss.insertRule('.coloredText { background: #E4FF00; }', 0);
+      ss.insertRule('a { cursor: url(./images/cursors/yellow_icon.png) 0 68, auto; }', 0);
+      document.getElementById("logoPic").src = "./images/logos/yellow@2x.png";
 
+   }
+   if(colorPick==2){
+      document.body.style.cursor = "url(./images/cursors/green_icon.png) 0 68, auto";
+      ss.insertRule('.coloredText { background: #7CFB6D; }', 0);
+      ss.insertRule('a { cursor: url(./images/cursors/green_icon.png) 0 68, auto; }', 0);
+      document.getElementById("logoPic").src = "./images/logos/green@2x.png";
   }
-  if(colorPick==2){
-    document.body.style.cursor = "url(./images/cursors/green_icon.png) 0 68, auto";
-    ss.insertRule('.coloredText { background: #7CFB6D; }', 0);
-    ss.insertRule('a { cursor: url(./images/cursors/green_icon.png) 0 68, auto; }', 0);
-    document.getElementById("logoPic").src = "./images/logos/green@2x.png";
-  }
-  if(colorPick==3){
-    document.body.style.cursor = "url(./images/cursors/blue_icon.png) 0 68, auto";
-    ss.insertRule('.coloredText { background: #72D3FD; }', 0);
-    ss.insertRule('a { cursor: url(./images/cursors/blue_icon.png) 0 68, auto; }', 0);
-    document.getElementById("logoPic").src = "./images/logos/blue@2x.png";
-  }
-  if(colorPick==4){
-    document.body.style.cursor = "url(./images/cursors/pink_icon.png) 0 68, auto";
-    ss.insertRule('.coloredText { background: #FF8EC9; }', 0);
-    ss.insertRule('a { cursor: url(./images/cursors/pink_icon.png) 0 68, auto; }', 0);
-    document.getElementById("logoPic").src = "./images/logos/pink@2x.png";
-  }
-  if(colorPick==5){
-    document.body.style.cursor = "url(./images/cursors/purple_icon.png) 0 68, auto";
-    ss.insertRule('.coloredText { background: #B25AFF; }', 0);
-    ss.insertRule('a { cursor: url(./images/cursors/purple_icon.png) 0 68, auto; }', 0);
-    document.getElementById("logoPic").src = "./images/logos/purple@2x.png";
-  }
-  if(colorPick==6){
-    document.body.style.cursor = "url(./images/cursors/orange_icon.png) 0 68, auto";
-    ss.insertRule('.coloredText { background: #FEB751; }', 0);
-    ss.insertRule('a { cursor: url(./images/cursors/orange_icon.png) 0 68, auto; }', 0);
-    document.getElementById("logoPic").src = "./images/logos/orange@2x.png";
-  }
+    if(colorPick==3){
+      document.body.style.cursor = "url(./images/cursors/blue_icon.png) 0 68, auto";
+      ss.insertRule('.coloredText { background: #72D3FD; }', 0);
+      ss.insertRule('a { cursor: url(./images/cursors/blue_icon.png) 0 68, auto; }', 0);
+      document.getElementById("logoPic").src = "./images/logos/blue@2x.png";
+   }
+   if(colorPick==4){
+      document.body.style.cursor = "url(./images/cursors/pink_icon.png) 0 68, auto";
+      ss.insertRule('.coloredText { background: #FF8EC9; }', 0);
+      ss.insertRule('a { cursor: url(./images/cursors/pink_icon.png) 0 68, auto; }', 0);
+      document.getElementById("logoPic").src = "./images/logos/pink@2x.png";
+   }
+   if(colorPick==5){
+      document.body.style.cursor = "url(./images/cursors/purple_icon.png) 0 68, auto";
+      ss.insertRule('.coloredText { background: #B25AFF; }', 0);
+      ss.insertRule('a { cursor: url(./images/cursors/purple_icon.png) 0 68, auto; }', 0);
+      document.getElementById("logoPic").src = "./images/logos/purple@2x.png";
+   }
+   if(colorPick==6){
+      document.body.style.cursor = "url(./images/cursors/orange_icon.png) 0 68, auto";
+      ss.insertRule('.coloredText { background: #FEB751; }', 0);
+      ss.insertRule('a { cursor: url(./images/cursors/orange_icon.png) 0 68, auto; }', 0);
+      document.getElementById("logoPic").src = "./images/logos/orange@2x.png";
+    }
   }else{
   console.log(colorPick);
   if(colorPick==1){
